022daf0810b5cac1a69d9a2db6e09e73bb0ff68f
Gobzi <jnlazaridis@gmail.com>
2024-07-31 15:59:21 +0300

    Update tls0r.py

--- tls0r.py
+++ tls0r.py
@@ -80,6 +80,34 @@ if args.generate:
         'RC4': 'RC4'
     }
 
+    weak_cipher_suites_id = [
+        'cipherlist_NULL', 'cipherlist_aNULL', 'cipherlist_EXPORT', 'cipherlist_LOW',
+        'cipherlist_3DES_IDEA', 'cipherlist_OBSOLETED', 'cipherlist_STRONG_NOFS',
+        'cipherlist_STRONG_FS'
+    ]
+
+    certificate_issues_id = [
+        'cert_numbers', 'cert_signatureAlgorithm', 'cert_keySize', 'cert_keyUsage',
+        'cert_extKeyUsage', 'cert_serialNumber', 'cert_serialNumberLen', 'cert_fingerprintSHA1',
+        'cert_fingerprintSHA256', 'cert', 'cert_commonName', 'cert_commonName_wo_SNI',
+        'cert_subjectAltName', 'cert_trust', 'cert_chain_of_trust', 'cert_certificatePolicies_EV',
+        'cert_expirationStatus', 'cert_notBefore', 'cert_notAfter', 'cert_extlifeSpan',
+        'cert_eTLS', 'cert_crlDistributionPoints', 'cert_ocspURL', 'OCSP_stapling',
+        'cert_ocspRevoked', 'cert_mustStapleExtension', 'DNS_CAArecord', 'certificate_transparency',
+        'certs_countServer', 'certs_list_ordering_problem', 'cert_caIssuers',
+        'intermediate_cert', 'intermediate_cert_fingerprintSHA256', 'intermediate_cert_notBefore',
+        'intermediate_cert_notAfter', 'intermediate_cert_expiration', 'intermediate_cert_chain'
+    ]
+
+    HSTS_id = [
+        'HSTS', '', 'HSTS_preload', 'HSTS_time',
+        'HSTS_subdomains'
+    ]
+
+    allowed_ids_mapping['weak_cipher_suites'] = 'Weak Cipher Suites'
+    allowed_ids_mapping['certificate_issues'] = 'Certificate Issues'
+    allowed_ids_mapping['HSTS'] = 'HSTS Issues'
+
     header_font = Font(name='Arial', bold=True, size=11)
     header_alignment = Alignment(horizontal='center', vertical='center')
 
@@ -102,17 +130,45 @@ if args.generate:
         cell.alignment = header_alignment
 
     findings_dict = {}
+    weak_cipher_ids = set(weak_cipher_suites_id)
+    certificate_issues_ids = set(weak_cipher_suites_id)
+    HSTS_ids = set(HSTS_id)
     csv_files = glob.glob(os.path.join('.', '*.csv'))
     for csv_file_path in csv_files:
         df = pd.read_csv(csv_file_path)
         filtered_df = df[~df['severity'].isin(['OK', 'INFO'])]
         for _, row in filtered_df.iterrows():
-            if row['id'] in allowed_ids_mapping:
+            if row['id'] in allowed_ids_mapping or row['id'] in weak_cipher_ids:
                 ip = row['fqdn/ip'].split('/')[0]
                 ip_port = f"{ip}:{row['port']}"
                 if ip_port not in findings_dict:
-                    findings_dict[ip_port] = {allowed_ids_mapping[id]: 'Secure' for id in allowed_ids_mapping}
-                findings_dict[ip_port][allowed_ids_mapping[row['id']]] = 'Vulnerable'
+                    findings_dict[ip_port] = {allowed_ids_mapping.get(id, 'Secure'): 'Secure' for id in allowed_ids_mapping}
+                if row['id'] in weak_cipher_ids:
+                    findings_dict[ip_port]['Weak Cipher Suites'] = 'Vulnerable'
+                else:
+                    findings_dict[ip_port][allowed_ids_mapping[row['id']]] = 'Vulnerable'
+
+        for _, row in filtered_df.iterrows():
+            if row['id'] in allowed_ids_mapping or row['id'] in certificate_issues_ids:
+                ip = row['fqdn/ip'].split('/')[0]
+                ip_port = f"{ip}:{row['port']}"
+                if ip_port not in findings_dict:
+                    findings_dict[ip_port] = {allowed_ids_mapping.get(id, 'Secure'): 'Secure' for id in allowed_ids_mapping}
+                if row['id'] in certificate_issues_ids:
+                    findings_dict[ip_port]['Certificate Issues'] = 'Vulnerable'
+                else:
+                    findings_dict[ip_port][allowed_ids_mapping[row['id']]] = 'Vulnerable'
+
+        for _, row in filtered_df.iterrows():
+            if row['id'] in allowed_ids_mapping or row['id'] in HSTS_ids:
+                ip = row['fqdn/ip'].split('/')[0]
+                ip_port = f"{ip}:{row['port']}"
+                if ip_port not in findings_dict:
+                    findings_dict[ip_port] = {allowed_ids_mapping.get(id, 'Secure'): 'Secure' for id in allowed_ids_mapping}
+                if row['id'] in certificate_issues_ids:
+                    findings_dict[ip_port]['HSTS Issues'] = 'Vulnerable'
+                else:
+                    findings_dict[ip_port][allowed_ids_mapping[row['id']]] = 'Vulnerable'
 
     for ip_port, findings in findings_dict.items():
         row_data = [ip_port] + [findings.get(formatted_id, 'Secure') for formatted_id in headers[1:]]
@@ -137,4 +193,4 @@ if args.generate:
         ws.column_dimensions[column[0].column_letter].width = adjusted_width
     output_file_path = "../TLS_report.xlsx"
     wb.save(output_file_path)
-    print(f"TLS Report Spreadsheet generated: {output_file_path}")
+    print(f"TLS Report Spreadsheet generated: TLS_report.xlsx")
